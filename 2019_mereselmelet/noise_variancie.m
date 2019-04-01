fe868a17060f8b3c57383f1571b7e501fef58e5a
majerimi <48290099+majerimi@users.noreply.github.com>
2019-04-01 21:37:45 +0200

    1.6 ready
    1.6 ready

--- 2019_mereselmelet/noise_variancie.m
+++ 2019_mereselmelet/noise_variancie.m
@@ -1,8 +1,8 @@
-function [variancie,varvariancie] = noise_variancie(A,B,C,w,a)
-global t0 dt f0 N;
+function [variancie,CRLB] = noise_variancie(A,B,C,w,a)
+global t0 dt f0 N sw;
 
 variancie=zeros(3,1);
-varvariancie=zeros(3,1);
+CRLB=zeros(3,1);
 
 for i=1:3
    t=t0:dt:(t0+dt*(N(i)-1));
@@ -12,6 +12,6 @@ for i=1:3
    U=a(i,1)*sin(2*pi*f0*t)+ a(i,2)*cos(2*pi*f0*t)+ a(i,3)*ones(1,N(i));
    % variancia szamitas
    variancie(i) = var(U_t-U);
-   varvariancie(i) = var(var(U_t-U));
+   CRLB(i) = (sw^2 / N(i));
 end
 end
